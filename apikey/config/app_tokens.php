5c09c6a748accb14faab115aeb54758299b2bcd5
pcualmac <jfpum.cal@gmail:.com>
2024-04-26 16:07:21 +0100

    add key for the apps

---
+++ apikey/config/app_tokens.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'application1' => env('JWT_SECRET_APP1', 'default_secret_key_for_app1'),
+    'application2' => env('JWT_SECRET_APP2', 'default_secret_key_for_app2'),
+    // Add more applications as needed...
+];
